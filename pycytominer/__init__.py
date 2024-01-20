fabf0fa8d39cd2b5b855358fe283c401788737ec
d33bs <dave.bunten@cuanschutz.edu>
2024-01-20 16:24:31 -0700

    set copy_on_write mode true for pandas

--- pycytominer/__init__.py
+++ pycytominer/__init__.py
@@ -4,3 +4,10 @@ from .feature_select import feature_select
 from .normalize import normalize
 from .consensus import consensus
 from pycytominer import __about__
+
+import pandas as pd
+
+# set copy_on_write for pandas to True
+# see here for more details:
+# https://pandas.pydata.org/pandas-docs/version/2.2.0/whatsnew/v2.2.0.html#copy-on-write
+pd.options.mode.copy_on_write = True
